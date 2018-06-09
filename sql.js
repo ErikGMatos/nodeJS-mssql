ecdec70e5bfeeeb1debc50786487643b3f7eece3
Erik Garces Matos <erik_senac@hotmail.com>
2018-06-08 21:04:42 -0300

    Update sql.js

--- sql.js
+++ sql.js
@@ -4,8 +4,8 @@ var app = express();
     app.get('/', async function(req, res){
         var sql = require('mssql');
         try {
-        const pool = await sql.connect('mssql://sa:homologacao@201.45.136.166:49700/CaptacaoConversaoGraduacao')
-        const result = await sql.query`select * from adm.pessoa where sexo ='M'  and email = 'ti@saolucas.edu.br'`
+        const pool = await sql.connect('mssql://sa:password@111.11.111.111:11111/basedados')
+        const result = await sql.query`select * from adm.pessoa where sexo ='M'`
         res.status(200).send(result); 
     } catch (err) {
         res.status(200).send(err); 
